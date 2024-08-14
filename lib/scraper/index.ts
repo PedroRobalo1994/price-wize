134bdb88928e57ada970dc74cf512aba1b2036a4
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2024-08-14 21:20:44 +0100

    Update scraper to extract currency and description

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,7 +1,7 @@
 'use server'
 import axios from 'axios'
 import * as cheerio from 'cheerio'
-import { extractPrice } from '../utils'
+import { extractCurrency, extractDescription, extractPrice } from '../utils'
 
 export async function scrapeAmazonProduct(productUrl: string) {
     if(!productUrl) return 
@@ -44,7 +44,37 @@ export async function scrapeAmazonProduct(productUrl: string) {
 
         const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable'
 
-        console.log(response.data)
+        const images = $('#imgBlkFront').attr('data-a-dynamic-image') || $('#landingImage').attr('data-a-dynamic-image') || '{}'
+
+        const imageUrls = Object.keys(JSON.parse(images))
+
+        const currency = extractCurrency($('.a-price-symbol'))
+
+        const discountRate = $('savingsPercentage').text().replace(/[-%]/g, '')
+
+        const description = extractDescription($)
+
+        // Construct data object with scrapped information
+        const productData = {
+            productUrl,
+            currency: currency || 'USD',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice)
+        }
+
+        return productData
     } catch (error: any) {
         throw new Error(`Failed to scrape product ${productUrl}: ${error.message}`)
         
